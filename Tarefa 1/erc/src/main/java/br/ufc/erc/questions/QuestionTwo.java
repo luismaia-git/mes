10fa065e50de8160c6cbdf3f3d68bf9491770013
maial <luismaia1407@gmail.com>
2024-04-03 19:50:02 -0300

    questoes 1, 2 e 3. adicionei alguns metodos da lib Spoon

---
+++ Tarefa 1/erc/src/main/java/br/ufc/erc/questions/QuestionTwo.java
@@ -0,0 +1,50 @@
+package br.ufc.erc.questions;
+
+import spoon.Launcher;
+import spoon.SpoonAPI;
+import spoon.reflect.CtModel;
+import spoon.reflect.declaration.CtElement;
+import spoon.reflect.declaration.CtType;
+import spoon.reflect.reference.CtTypeReference;
+import spoon.reflect.visitor.filter.TypeFilter;
+
+import java.util.*;
+
+public class QuestionTwo {
+    public static void main(String[] args) {
+
+        SpoonAPI spoon = new Launcher();
+        spoon.addInputResource("./res/banksys/src/main/java/");
+        spoon.buildModel();
+
+        HashMap<String, Set<String>> adjacencyList = new HashMap<>();
+
+        CtModel model = spoon.getModel();
+
+        //obtendo todos os tipos
+        Collection<CtType<?>> types = model.getAllTypes();
+        for(CtType<?> type : types) {
+            String typeName = type.getQualifiedName();
+            Set<String> referencedTypes = new HashSet<>();
+            if (!type.isShadow()) {
+                for (CtElement referredTypeCtElement : type.getDirectChildren()) {
+                    for (CtTypeReference<?> referredType : referredTypeCtElement.getReferencedTypes()) {
+                        if (!referredType.isShadow()) {
+                            if (!referredType.getQualifiedName().equals(typeName)) {
+                                referencedTypes.add(referredType.getQualifiedName());
+                            }
+                        }
+                }
+                adjacencyList.put(typeName, referencedTypes);
+            }
+        }
+
+            for (Map.Entry<String, Set<String>> entry : adjacencyList.entrySet()) {
+                System.out.println("Tipos: " + entry.getKey());
+                System.out.println("Herança : " + entry.getValue());
+                System.out.println();
+            }
+        }
+
+    }
+}
